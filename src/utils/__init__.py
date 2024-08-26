1e0c68488183465531c4bb14fecd9e27863318a3
Kauan Cardoso <kauancardososilva@gmail.com>
2024-08-26 18:11:08 -0400

    cleanup(swap): remocao da funcao swap, pois o python já tem suporte a swap em um array

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,4 @@
-from .swap import swap
 from .execution_time import measure_execution_time
 from .use_cases import UseCases
 
-__all__ = ["swap", "measure_execution_time", "UseCases"]
+__all__ = ["measure_execution_time", "UseCases"]
