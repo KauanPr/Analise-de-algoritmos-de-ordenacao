690e6b3218c53f0745a2d98c1d67066be926fe8d
Kauan Cardoso <kauancardososilva@gmail.com>
2024-09-15 00:30:11 -0400

    feat(create_graph): gerador de graficos

--- src/utils/graph_creator.py
+++ src/utils/graph_creator.py
@@ -1,24 +1,15 @@
 import matplotlib.pyplot as plt
-from src.utils.table_class import Table
+from .table_class import Table
+import os
 
-def create_graph(tables: Table, table_name: str):
-    
+
+def create_graph(table_instance: Table, table_name: str, algorithms_in_graph: [] = None, graph_name: str = None,
+                 folder_name: str = None):
     # obter a tabela passada nos parametros
-    table = tables.tables[table_name]
+    table = table_instance.tables[table_name]
 
-    
     # Separar os dados da tabela
     n_values = [line[0] for line in table]  # Coluna de tamanhos de array 'n'
-    
-    # colocar os tempos em um dicionario
-    algorithms_times = {
-        "bubble_times": [],
-        "insertion_times": [],
-        "merge_times": [],
-        "heap_times": [],
-        "quick_times": [],
-        "counting_times": []
-    }
 
     bubble_sort_times = [line[1] for line in table]
     insertion_sort_times = [line[2] for line in table]
@@ -27,24 +18,37 @@ def create_graph(tables: Table, table_name: str):
     quick_sort_times = [line[5] for line in table]
     counting_sort_times = [line[6] for line in table]
 
+    sort_times = [bubble_sort_times, insertion_sort_times, merge_sort_times, heap_sort_times, quick_sort_times,
+                  counting_sort_times]
+
     # Criando o gráfico
     plt.figure(figsize=(10, 6))
 
     # Plotar os tempos para cada algoritmo
-    plt.plot(n_values, bubble_sort_times, label='Bubble Sort', marker='o')
-    plt.plot(n_values, insertion_sort_times, label='Insertion Sort', marker='o')
-    plt.plot(n_values, merge_sort_times, label='Merge Sort', marker='o')
-    plt.plot(n_values, heap_sort_times, label='Heap Sort', marker='o')
-    plt.plot(n_values, quick_sort_times, label='Quick Sort', marker='o')
-    plt.plot(n_values, counting_sort_times, label='Counting Sort', marker='o')
+
+    # Caso não seja definido quais algoritmos devem ser colocados no gráfico, todos os algoritmos serão colocados
+    if algorithms_in_graph is None:
+        algorithms_in_graph = ['bubble', 'insertion', 'merge', 'heap', 'quick', 'counting']
+
+    for algorithm_time, algorithm in zip(sort_times, table_instance.algorithms_names):
+        if algorithm in algorithms_in_graph:
+            plt.plot(n_values, algorithm_time, label=str(algorithm + '_sort'), marker='o')
 
     # Configurações do gráfico
     plt.title(f'Tempo de execução dos algoritmos de ordenação || {table_name.upper()}')
     plt.xlabel('Tamanho do Array (n)')
     plt.ylabel('Tempo de Execução (segundos)')
-    plt.legend()  # Exibe a legenda para identificar os algoritmos
+    plt.legend(algorithms_in_graph)  # Exibe a legenda para identificar os algoritmos
     plt.grid(True)
 
-    # Exibe o gráfico
-    plt.show()
+    # Salvar o gráfico caso seja passado o local de salvemnto pelo parâmetro
+    if graph_name is not None and folder_name is not None:
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+        address_file = os.path.join(folder_name, graph_name)
+        plt.savefig(address_file)
 
+    # Exibe o gráfico
+    #plt.show()
+
+    plt.close()
